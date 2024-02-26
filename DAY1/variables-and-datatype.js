781d3bc306a342b66e34d99b03accda00a9f2e78
Nyiranez <nyiranezalouise93@gmail.com>
2024-02-26 13:10:12 +0200

    my javascript codes

---
+++ DAY1/variables-and-datatype.js
@@ -0,0 +1,17 @@
+// types of variables
+// to day i'm going to focus on variables and datatypes
+
+// 1. Numeric
+// 2. boalian
+
+
+const age= 18;
+const myName="louise";
+
+console.log("my name is"+ " " + myName + " " + "and i'm "+" " + age+" " + "years old");
+
+function addMe(){
+    let me= "Louise"
+    console.log(me);
+}
+addMe();
